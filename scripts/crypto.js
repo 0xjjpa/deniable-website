4d581ba7a913f63ad126e258fcb422efac70598e
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2017-02-28 20:40:08 +0000

    Compiled version

--- scripts/crypto.js
+++
@@ -1,7 +0,0 @@
-const encrypt = (data, key, iv, mode) =>
-    (window.crypto.subtle||window.crypto.webkitSubtle).importKey('raw', key, {name: mode}, true, ['encrypt', 'decrypt'])
-        .then(bufKey => (window.crypto.subtle||window.crypto.webkitSubtle).encrypt({name: mode, iv}, bufKey, data))
-
-const decrypt = (data, key, iv, mode) =>
-    (window.crypto.subtle||window.crypto.webkitSubtle).importKey('raw', key, {name: mode}, true, ['encrypt', 'decrypt'])
-        .then(bufKey => (window.crypto.subtle||window.crypto.webkitSubtle).decrypt({name: mode, iv}, bufKey, data))
